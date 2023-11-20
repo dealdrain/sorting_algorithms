4e11aa2bc65bbefea08db8287fad36e545576dca
dealdrain <dealdrain@yahoo.com>
2023-11-20 08:21:03 -0800

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,7 +4,7 @@
  * bubble_sort - sorting an array of ints
  * @array: the array
  * @size: array size
- * int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+ *
  */
 
 void bubble_sort(int *array, size_t size)
