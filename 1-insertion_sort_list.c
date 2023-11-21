f6348f3f68663ab22a27f8f0a876b3298c455a70
dealdrain <dealdrain@yahoo.com>
2023-11-21 01:20:15 -0800

    1. Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,49 +6,44 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *next_node, *aux;
+	listint_t *current, *next_node, *aux;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+	if (!list || !(*list) || !(*list)->next)
+		return;
+	current = *list;
+	while (current->next)
+	{
+		next_node = current->next;
+		if (current->n > next_node->n)
+		{
+			next_node->prev = current->prev;
+			if (next_node->prev)
+				next_node->prev->next = next_node;
+			else
+				*list = next_node;
 
-    current = *list;
-    while (current->next)
-    {
-        next_node = current->next;
-        if (current->n > next_node->n)
-        {
-            next_node->prev = current->prev;
-            if (next_node->prev)
-                next_node->prev->next = next_node;
-            else
-                *list = next_node;
+			current->prev = next_node;
+			current->next = next_node->next;
+			next_node->next = current;
+			if (current->next)
+				current->next->prev = current;
+			current = next_node->prev;
+			print_list(*list);
+			while (current->prev && current->prev->n > current->n)
+			{
+				aux = current->next;
+				current->next = current->prev;
+				current->prev = current->next->prev;
+				current->next->prev = current;
+				current->next->next = aux;
 
-            current->prev = next_node;
-            current->next = next_node->next;
-            next_node->next = current;
-            if (current->next)
-                current->next->prev = current;
-
-            current = next_node->prev;
-            print_list(*list);
-
-            while (current->prev && current->prev->n > current->n)
-            {
-                aux = current->next;
-                current->next = current->prev;
-                current->prev = current->next->prev;
-                current->next->prev = current;
-                current->next->next = aux;
-
-                if (current->prev)
-                    current->prev->next = current;
-                else
-                    *list = current;
-
-                print_list(*list);
-            }
-        }
-        current = current->next;
-    }
+				if (current->prev)
+					current->prev->next = current;
+				else
+					*list = current;
+				print_list(*list);
+			}
+		}
+		current = current->next;
+	}
 }
-
