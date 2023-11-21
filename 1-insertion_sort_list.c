85c6b15588acfb93e63b7e659c4adfe6d9ae443c
dealdrain <dealdrain@yahoo.com>
2023-11-21 03:31:25 -0800

    1. Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,36 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorting a dl list (int)
- * @list: The DL list
+ * insertion_sort_list - sorting a DL list of int
+ * @list: DL list
+ * Return: void always
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *next_node, *aux;
+	listint_t *current = *list, *temp = NULL;
 
-	if (!list || !(*list) || !(*list)->next)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	current = *list;
-	while (current->next)
+
+	while (current)
 	{
-		next_node = current->next;
-		if (current->n > next_node->n)
+		while (current->prev && current->n < current->prev->n)
 		{
-			next_node->prev = current->prev;
-			if (next_node->prev)
-				next_node->prev->next = next_node;
-			else
-				*list = next_node;
-
-			current->prev = next_node;
-			current->next = next_node->next;
-			next_node->next = current;
+			temp = current;
 			if (current->next)
-				current->next->prev = current;
-			current = next_node->prev;
+				current->next->prev = temp->prev;
+			if (temp->prev)
+				temp->prev->next = temp->next;
+			current = current->prev;
+			temp->prev = current->prev;
+			temp->next = current;
+			if (current->prev)
+				current->prev->next = temp;
+			current->prev = temp;
+			if (temp->prev == NULL)
+				*list = temp;
 			print_list(*list);
-			while (current->prev && current->prev->n > current->n)
-			{
-				aux = current->next;
-				current->next = current->prev;
-				current->prev = current->next->prev;
-				current->next->prev = current;
-				current->next->next = aux;
-
-				if (current->prev)
-					current->prev->next = current;
-				else
-					*list = current;
-				print_list(*list);
-			}
+			current = current->prev;
 		}
 		current = current->next;
 	}
